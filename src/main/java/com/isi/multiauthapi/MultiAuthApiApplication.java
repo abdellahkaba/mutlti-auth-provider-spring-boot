877f1f18e8d7db23e01f08d4559357a5d977421c
Abdellah <abdallahkaba98@gmail.com>
2024-05-31 12:11:47 +0000

    propertie-class

--- src/main/java/com/isi/multiauthapi/MultiAuthApiApplication.java
+++ src/main/java/com/isi/multiauthapi/MultiAuthApiApplication.java
@@ -1,9 +1,12 @@
 package com.isi.multiauthapi;
 
+import com.isi.multiauthapi.configs.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class MultiAuthApiApplication {
 
     public static void main(String[] args) {
