2fdf04d095298ab8ba46786d8cf19cead23cefd2
Abdellah <abdallahkaba98@gmail.com>
2024-05-31 11:57:35 +0000

    simple-filtre-CORS

---
+++ src/main/java/com/isi/multiauthapi/MultiAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.isi.multiauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiAuthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isi/multiauthapi/MultiAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isi.multiauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
